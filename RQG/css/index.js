f007747f8b9eab01812563c88447c17c78af1478
Lars Behrenberg <32539217+LarsBehrenberg@users.noreply.github.com>
2017-10-30 15:09:39 +0900

    Added array for quotes; backgroundColor changes; twitterButton works;

--- RQG/css/index.js
+++ RQG/css/index.js
@@ -1,18 +1,41 @@
-var rndQuote = [{quote:"Most of our pocket wisdom is conceived for the use of mediocre people, to discourage them from ambitious attempts, and generally console them in their mediocrity.",author:"Robert Louis Stevenson",},{quote:"Poorer students take out larger loans and will have to contribute more to the cost of higher education.",author:"Anne Campbell",},{quote:"I think it's harder for people than it should be. But as more and more of us become carbon neutral and change the patterns in our lives to be part of the solution instead of part of the problem, we are now beginning to see the changes in policy that are needed.",author:"Al Gore",},{quote:"I am someone who hopes for the best and prepares for the worst.",author:"Hillary Clinton",},{quote:"That first year at Universal was a big blur and, naturally, I thought they were wasting me. I didn't realize at the time that I was learning my craft and acting more easily in front of the camera.", author:"Kent McCord"}]
-//Button.onclick
+//Variables for new Quote Button
+var rndQuote = rndQuotesJS;
+var rndColor = rndColorJS;
+var lastNumberQuotes = 0;
+var lastNumberColor = 0;
 
-var lastNumber = 0;
-
+//New Quote Button
 document.getElementById("newQuote").onclick = function() {myFunction()};
 function myFunction() {
-  var rndNumber = Math.floor(Math.random() * 3) + 0;
-  if(rndNumber != lastNumber){
-          document.getElementById("quotes").innerHTML = rndQuote[rndNumber].author+ " says: " + rndQuote[rndNumber].quote;
-          lastNumber = rndNumber;
+  //Get a randomQuote from quoteArray & randomColor from colorArray
+  var rndNumberQuote = Math.floor(Math.random() * rndQuote.length) + 0;
+  var rndNumberColor = Math.floor(Math.random() * rndColor.length) + 0;
+  //Display Quote and Author and making sure nothing repeats
+  if(rndNumberQuote != lastNumberQuotes && rndNumberColor != lastNumberColor){
+          document.getElementById("randomQuote").innerHTML = rndQuote[rndNumberQuote].quote;
+          document.getElementById("randomAuthor").innerHTML = rndQuote[rndNumberQuote].name;
+          lastNumberQuotes = rndNumberQuote;
+          lastNumberColor = rndNumberColor;
+          //Setting randomBackgroundColor & Button Color
+          document.getElementById("newTwitter").style.backgroundColor = rndColor[rndNumberColor];
+          document.getElementById("newQuote").style.backgroundColor = rndColor[rndNumberColor];
+          document.body.style.backgroundColor = rndColor[rndNumberColor];
       }else{
           myFunction();
       }
 
-
-
   }
+//Twitter Button
+document.getElementById("newTwitter").onclick = function() {twitter()};
+function twitter(){
+  var twitterquote = document.getElementById("randomQuote").innerHTML;
+  var twitterauthor =  document.getElementById("randomAuthor").innerHTML;
+  if (twitterauthor.length != 0){
+    var tweetUrl = 'https://twitter.com/intent/tweet?text="' + encodeURIComponent(twitterquote) + '"' + encodeURIComponent(" - " + twitterauthor);
+    window.open(tweetUrl);
+  }
+    else{
+      var tweetUrl = 'https://twitter.com/intent/tweet?text=' + 'This RandomQuoteGenerator is way cool!';
+      window.open(tweetUrl);
+    }
+}
