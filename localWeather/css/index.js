0301974faf904f69313648ea0509cee7ddc9030a
Lars Behrenberg <32539217+LarsBehrenberg@users.noreply.github.com>
2017-11-01 15:07:47 +0900

    Added googlemapsAPI and display of it

--- localWeather/css/index.js
+++ localWeather/css/index.js
@@ -1,29 +1,25 @@
-var x;
-var y;
+var x = 0;
+var y = 0;
 //Get position while loading window
 window.onload = function(){
       if (navigator.geolocation) {
-          navigator.geolocation.getCurrentPosition(showPosition);
+          navigator.geolocation.getCurrentPosition(showPosition, myMap);
+
       } else {
           x.innerHTML = "Geolocation is not supported by this browser.";
       }
-  function showPosition(position) {
-      document.getElementById("demo").innerHTML = "Latitude: " + position.coords.latitude +
-      "<br>Longitude: " + position.coords.longitude;
-       x = position.coords.latitude;
-       y = position.coords.longitude;
-
-  }
-
 }
 
-function showMap(){
-  function myMap(position) {
-    var mapProp= {
-        center:new google.maps.LatLng(position.coords.latitude,position.coords.longitude),
-        zoom:10,
-    };
-    var map=new google.maps.Map(document.getElementById("googleMap"),mapProp);
-    }
-    document.getElementById("test").innerHTML = "yes";
+
+function showPosition(position) {
+    document.getElementById("demo").innerHTML = "Latitude: " + position.coords.latitude +
+    "<br>Longitude: " + position.coords.longitude;
+}
+
+function myMap() {
+var mapProp= {
+    center:new google.maps.LatLng(1,-2),
+    zoom:5,
+};
+var map=new google.maps.Map(document.getElementById("googleMap"),mapProp);
 }
