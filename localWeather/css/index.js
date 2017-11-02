8a439fdd086f83360dafa85163503d7b71214cb7
Lars Behrenberg <32539217+LarsBehrenberg@users.noreply.github.com>
2017-11-02 14:44:55 +0900

    updated map and centers it on your location

--- localWeather/css/index.js
+++ localWeather/css/index.js
@@ -1,25 +1,27 @@
-var x = 0;
-var y = 0;
 //Get position while loading window
 window.onload = function(){
       if (navigator.geolocation) {
-          navigator.geolocation.getCurrentPosition(showPosition, myMap);
-
+          navigator.geolocation.getCurrentPosition(showPosition);
+          navigator.geolocation.getCurrentPosition(myMap);
       } else {
           x.innerHTML = "Geolocation is not supported by this browser.";
       }
 }
 
-
+//Displays coordinates in text
 function showPosition(position) {
     document.getElementById("demo").innerHTML = "Latitude: " + position.coords.latitude +
     "<br>Longitude: " + position.coords.longitude;
 }
 
-function myMap() {
+//Creates Map and centers map on your location
+function myMap(position) {
 var mapProp= {
-    center:new google.maps.LatLng(1,-2),
-    zoom:5,
+    center:new google.maps.LatLng(position.coords.latitude,position.coords.longitude),
+    zoom:10,
+    mapTypeId: google.maps.MapTypeId.SATELLITE
 };
 var map=new google.maps.Map(document.getElementById("googleMap"),mapProp);
 }
+
+getElementById("test").innerHTML = coord[0];
