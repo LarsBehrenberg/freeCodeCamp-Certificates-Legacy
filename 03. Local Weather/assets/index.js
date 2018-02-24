0aeb0ca77867a058de4f3093d68f072dcc6fb1f0
Lars Behrenberg <32539217+LarsBehrenberg@users.noreply.github.com>
2018-02-24 15:01:24 +0900

    Redoing the challenge

--- 03. Local Weather/assets/index.js
+++ 03. Local Weather/assets/index.js
@@ -1,87 +1,68 @@
-//Calling API and pushing it to <p>
-var xhttp = new XMLHttpRequest();
-var serverResponse = "";
-xhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-       // Action to be performed when the document is ready;
-       var parsed = JSON.parse(this.responseText);
-       document.getElementById("tempre").innerHTML = parsed.main.temp + "°C, " + parsed.weather[0].description;
-       if (parsed.weather[0].description == "scattered clouds") {
-         document.body.style.backgroundImage = parsed.weather[0].icon; //need to do something here!!!
-         //here!!
+var temperatureNum;
 
-       }
-    }
+//Stuff that happens when loading the site
+window.onload = function(){
 
-};
-
-
-
-//Geocoding begins here
-var geocoder;
-
-if (navigator.geolocation) {
-  navigator.geolocation.getCurrentPosition(successFunction, errorFunction);
-  navigator.geolocation.getCurrentPosition(myMap);
-}
-//Get the latitude and the longitude;
-function successFunction(position) {
-  //opening fccAPI
-  xhttp.open("GET","https://fcc-weather-api.glitch.me/api/current?lat=" + position.coords.latitude + "&lon=" + position.coords.longitude, true);
-  xhttp.send();
-  var lat = position.coords.latitude;
-  var lng = position.coords.longitude;
-  codeLatLng(lat, lng)
-}
-
-//creates errorMessage
-function errorFunction(){
-  alert("Geocoder failed");
-}
-
-// initializing Geocoding
-function initialize() {
-  geocoder = new google.maps.Geocoder();
-}
-
-function codeLatLng(lat, lng) {
-
-  var latlng = new google.maps.LatLng(lat, lng);
-  geocoder.geocode({'latLng': latlng}, function(results, status) {
-    if (status == google.maps.GeocoderStatus.OK) {
-    console.log(results)
-      if (results[1]) {
-       //formatted address
-       document.getElementById("demo1").innerHTML = results[0].formatted_address;
-       //alert(results[0].formatted_address)
-      //find country name
-           for (var i=0; i<results[0].address_components.length; i++) {
-          for (var b=0;b<results[0].address_components[i].types.length;b++) {
-
-          //there are different types that might hold a city admin_area_lvl_1
-          //usually does in come cases looking for sublocality type will be more appropriate
-              if (results[0].address_components[i].types[b] == "administrative_area_level_1") {
-                  //this is the object you are looking for
-                  city= results[0].address_components[i];
-                  break;
-              }
-          }
+    //GeoCoding from here on
+    function getLocation() {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(showPosition);
+        } else { 
+            alert("Geolocation is not supported by this browser.");
         }
-      } else {
-        alert("No results found");
-        }
-      } else {
-      alert("Geocoder failed due to: " + status);
     }
-  });
-}
+    
+    //Showing coordinates in a <p> element
+    function showPosition(position) {
+    
+        //Defining latitude and longitute for putting in the API link
+        var lat = position.coords.latitude;
+        var long = position.coords.longitude;
 
-//Creates Map and centers map on your location
-function myMap(position) {
-var mapProp= {
-    center:new google.maps.LatLng(position.coords.latitude,position.coords.longitude),
-    zoom:15,
-    //mapTypeId: google.maps.MapTypeId.SATELLITE
-};
-var map=new google.maps.Map(document.getElementById("googleMap"),mapProp);
+        //Declaring variable for XMLHttpRequest
+        var xml = new XMLHttpRequest;
+
+            //When Request ready then do this
+            xml.onreadystatechange = function(){
+        
+                //If request completed then do this
+                if (xml.readyState == 4 && xml.status == 200){
+            
+                    //Giving the different information to the elements
+                    var parsed = JSON.parse(xml.responseText);
+                    console.log(parsed);
+                    document.getElementById("location").innerHTML = parsed.name;
+                    document.getElementById("temperature").innerHTML = parsed.main.temp + "°C";
+                    document.getElementById("sky").innerHTML = parsed.weather[0].main;
+                    document.getElementById("icon").innerHTML = "<img src='" + parsed.weather[0].icon + "'>";
+                    document.getElementById("author").innerHTML = "Designed by ラース";
+                    document.getElementById("buttondiv").innerHTML = "<button onclick='button()'id='button'>Celsius/Fahrenheit</button>";
+
+                    //Declaring the temp as a variable for later use with the button
+                    temperatureNum = parsed.main.temp;
+                }   
+            }      
+                //Request to the API
+                xml.open("GET","https://fcc-weather-api.glitch.me/api/current?lat=" + lat + "&lon=" + long, true);
+                xml.send();
+
+                
+    }
+    getLocation();  //Executing getLocation()
+}   //End of window.onload = function();
+
+//Celsius / Fahrenheit Button Switch
+var switchbutton = 0;
+
+function button() {
+    if(switchbutton == 0){
+        var fahrenheit = (temperatureNum*1.8) + 32;
+        document.getElementById("temperature").innerHTML = fahrenheit.toFixed(2) + " °F";
+        switchbutton = 1;
+    }
+    else if(switchbutton == 1){
+        var celsius = temperatureNum;
+        document.getElementById("temperature").innerHTML = celsius.toFixed(2) + " °C";
+        switchbutton = 0;
+    }
 }
