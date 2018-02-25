a04fb644171442d8cebfbd0a70506aa0d32567be
Lars Behrenberg <32539217+LarsBehrenberg@users.noreply.github.com>
2018-02-25 22:33:39 +0900

    Comments

--- 04. Wikipedia Viewer/assets/index.js
+++ 04. Wikipedia Viewer/assets/index.js
@@ -3,57 +3,57 @@ var xhrParsed;
 var newDiv;
 
 function searchInput() {
-    var searchFieldInput = document.forms["myForm"]["searchField"].value;
-    if (searchFieldInput == "") {
-        alert("Please type something");
+    var searchFieldInput = document.forms["myForm"]["searchField"].value;   // Taking the input from search and set as variable
+    if (searchFieldInput == "") {                                           // If Search field is empty, but triggered
+        alert("Please type something");                                     // Then giving an alert and return false
         return false;
     }
-    else{
-        console.log(searchFieldInput);
+    else{                                                                   // Otherwise if something is typed in then this triggers
 
+        //API Request Link    
         var endpoint = "https://en.wikipedia.org/w/api.php?action=query&format=json&list=search&utf8=1&origin=*&srsearch=" + searchFieldInput;
 
-    var xhr = new XMLHttpRequest;
+        var xhr = new XMLHttpRequest;
 
-    xhr.onreadystatechange = function() { //checking everytime request state is changing
-       
-        if (xhr.readyState == 4 && xhr.status == 200) { //runs when request state is == 4 meaning completed
-            xhrParsed = JSON.parse(xhr.response);
-            var element = document.getElementById("second");
-            
+        xhr.onreadystatechange = function() { //checking everytime request state is changing
+        
+            if (xhr.readyState == 4 && xhr.status == 200) {     // Runs when request state is == 4 meaning completed
+                xhrParsed = JSON.parse(xhr.response);           // Parse the API response and setting as variable for later use
+                var element = document.getElementById("second");// Setecting div #second and the as variable
+                
 
-            if(element.querySelectorAll("p").length == 0) {
-                for (var x = 0; x < xhrParsed.query.search.length; x++) {
-                    newDiv = document.createElement("p"); // Create the div
-                    newDiv.setAttribute("id", x);
-                    newDiv.setAttribute("class", "result")
-    
-                    element.appendChild(newDiv);
-                    document.getElementById(x).innerHTML = "<a target='_blank' href='https://en.wikipedia.org/?curid=" + xhrParsed.query.search[x].pageid + "'><strong>" + xhrParsed.query.search[x].title + "</strong><br>" + "<br>" + xhrParsed.query.search[x].snippet + "...</a>";   
+                if(element.querySelectorAll("p").length == 0) { // This triggers when no search happened before, therefore no p elements exist in #second yet
+                    for (var x = 0; x < xhrParsed.query.search.length; x++) {   // goes through the API.length
+                        newDiv = document.createElement("p");   // Create the div
+                        newDiv.setAttribute("id", x);           // Setting the individual id to the elements
+                        newDiv.setAttribute("class", "result")  // Giving the element the result class for designing with CSS
+        
+                        element.appendChild(newDiv);            // Adding the newDiv to the div #second
+
+                        // Adding the entry title & Snippet to the individual x elements
+                        document.getElementById(x).innerHTML = "<a target='_blank' href='https://en.wikipedia.org/?curid=" + xhrParsed.query.search[x].pageid + "'><strong>" + xhrParsed.query.search[x].title + "</strong><br>" + "<br>" + xhrParsed.query.search[x].snippet + "...</a>";   
+                    }
                 }
-                    // document.getElementById("entryTitle" + x).innerHTML = xhrParsed.query.search[x].title + "<br>" + "<br>" + xhrParsed.query.search[x].snippet;    
-            }
-            else{
-                while (element.firstChild) {
-                    element.removeChild(element.firstChild);
-                }
+                // This triggers when there was a search before so divs need to be deleted first before recreating new ones
+                else{ 
+                    while (element.firstChild) {                // while div #second still has a child
+                        element.removeChild(element.firstChild);// remove this child
+                    }
 
-                for (var x = 0; x < xhrParsed.query.search.length; x++) {
-                    newDiv = document.createElement("p"); // Create the div
-                    newDiv.setAttribute("id", x);
-                    newDiv.setAttribute("class", "result")
-    
-                    element.appendChild(newDiv);
-                    document.getElementById(x).innerHTML = "<a target='_blank' href='https://en.wikipedia.org/?curid=" + xhrParsed.query.search[x].pageid + "'><strong>" + xhrParsed.query.search[x].title + "</strong><br>" + "<br>" + xhrParsed.query.search[x].snippet + "...</a>";   
-                }
-            }
-            
-            console.log(xhrParsed);
-            console.log(endpoint)
-        } // IF Clause
-    } //onreadystatechange
+                    for (var x = 0; x < xhrParsed.query.search.length; x++) { //same from here as from line 26
+                        newDiv = document.createElement("p");   // Create the div
+                        newDiv.setAttribute("id", x);           // Setting the individual id to the elements
+                        newDiv.setAttribute("class", "result")  // Giving the element the result class for designing with CSS
+        
+                        element.appendChild(newDiv);            // Adding the newDiv to the div #second
 
-    xhr.open("GET",endpoint, true);
-    xhr.send();
+                        // Adding the entry title & Snippet to the individual x elements
+                        document.getElementById(x).innerHTML = "<a target='_blank' href='https://en.wikipedia.org/?curid=" + xhrParsed.query.search[x].pageid + "'><strong>" + xhrParsed.query.search[x].title + "</strong><br>" + "<br>" + xhrParsed.query.search[x].snippet + "...</a>";   
+                    }
+                }
+            } // if readyState && status
+        } //onreadystatechange
+        xhr.open("GET",endpoint, true);
+        xhr.send();
     }
 }
